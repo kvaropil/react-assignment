52571ce3875552bd873928ca4780410080a228dc
Roman Kvapil <kvapilrom@gmail.com>
2023-08-30 17:59:11 +0200

    feat: Extend styled-comp. theme type definitions

---
+++ src/types/styled.d.ts
@@ -0,0 +1,15 @@
+import 'styled-components';
+
+// Extend the default theme type
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colors: {
+      orange: string;
+      beige: string;
+      white: string;
+      darkBlue: string;
+      darkGrey: string;
+      green: string;
+    };
+  }
+}
