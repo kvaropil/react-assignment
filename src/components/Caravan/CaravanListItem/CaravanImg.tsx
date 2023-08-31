0099d4baaa5bdcd51360472e30e5070a43e4a3c9
Roman Kvapil <kvapilrom@gmail.com>
2023-08-31 17:11:23 +0200

    feat: Implement CaravanImg component

---
+++ src/components/Caravan/CaravanListItem/CaravanImg.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledCaravanImg = styled.div`
+  border-top-right-radius: 8px;
+  border-top-left-radius: 8px;
+  height: 50%;
+  width: 100%;
+  margin-bottom: 12px;
+  background-size: cover; // ensure that image covers the div without stretching
+  background-position: center; // center image in the div
+`;
+
+type CaravanListItemImgProps = {
+  imgs: string[];
+};
+
+export const CaravanListItemImg: React.FC<CaravanListItemImgProps> = ({
+  imgs,
+}) => {
+  return (
+    <StyledCaravanImg
+      style={{ backgroundImage: `url(${imgs[0]})` }}
+    ></StyledCaravanImg>
+  );
+};
