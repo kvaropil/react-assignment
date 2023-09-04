1a2ea7b657425190a5edd2213ed99a14762aea3b
Roman Kvapil <kvapilrom@gmail.com>
2023-09-04 15:26:20 +0200

    fix: Add missing Kč placeholder

--- src/components/common/TextboxPrice.tsx
+++ src/components/common/TextboxPrice.tsx
@@ -3,24 +3,44 @@ import React from 'react';
 import { TextboxProps } from '../../types';
 import styled from 'styled-components';
 
-const StyledTextbox = styled(NumericFormat)`
+const TextboxWrapper = styled.div`
+  position: relative;
   width: 156px;
-  height: 48px;
+`;
+
+const StyledTextbox = styled(NumericFormat)`
   border: 1px solid ${(props) => props.theme.colors.beige};
+
   border-radius: 8px;
   font-size: 16px;
+  height: 48px;
   padding-left: 12px;
+  padding-right: 21px;
+  width: 100%;
+`;
+
+const Suffix = styled.span`
+  color: ${(props) => props.theme.colors.darkGrey};
+
+  font-size: 16px;
+  position: absolute;
+  right: 12px;
+  top: 50%;
+  transform: translateY(-50%);
 `;
 
 export const TextboxPrice: React.FC<TextboxProps> = ({ value }) => {
   // TODO: get rid of readonly, implement two-way data binding
-  // TODO: add "Kč" placeholder
+
   return (
-    <StyledTextbox
-      value={value}
-      displayType={'input'}
-      thousandSeparator={' '}
-      readOnly
-    />
+    <TextboxWrapper>
+      <StyledTextbox
+        value={value}
+        displayType={'input'}
+        thousandSeparator={' '}
+        readOnly
+      />
+      <Suffix>Kč</Suffix>
+    </TextboxWrapper>
   );
 };
